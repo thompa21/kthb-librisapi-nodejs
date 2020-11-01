c47a5b0b66324b617532281cc1484970d230592c
Thomas Lind <thomas.lind@tlcgolfit.se>
2020-11-01 23:05:16 +0100

    commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 const VerifyToken = require('./VerifyToken');
+const libris = require('./libris')
 
 const app = express();
 
@@ -36,49 +37,57 @@ apiRoutes.get('/', function(req, res) {
 	res.send('Hello! The API is at https://lib.kth.se/libris/api/v1');
 });
 
-apiRoutes.get("/librisholding/", VerifyToken, function(req , res, next){
+apiRoutes.get("/librisholding/", VerifyToken, async function(req , res, next){
 	
-	console.log(req.body.bib.network_number.indexOf('(LIBRIS)'))
-	//Finns "(LIBRIS)"=
-	/*
-	if(req.body.bib.network_number.indexOf('(LIBRIS)') !== -1 ) {
-		res.json({"currentbibid" : req.body.bib.network_number[req.body.bib.network_number.indexOf('(LIBRIS)')]});
-	}
-	*/
+	let currentid = "";
+	//Finns "(LIBRIS)"?
 	for (let k = 0; k < req.body.bib.network_number.length; k++) {
 		if(req.body.bib.network_number[k].indexOf('(LIBRIS)') !== -1 ) {
-			currentid = req.body.bib.network_number[k]
+			currentid = req.body.bib.network_number[k].substr(8, req.body.bib.network_number[k].length);//req.body.bib.network_number[k]
 			res.json({"currentbibid" : currentid});
+			response4 = await libris.findHoldinguri(currentid,'bibid')
 			break;
 		}
-		//response4 = await libris.findHoldinguri(librisidarr[k],'libris3')
+		
 	}
-	//Finns ett värde som saknar "("?
-	for (let k = 0; k < req.body.bib.network_number.length; k++) {
-		if(req.body.bib.network_number[k].indexOf('(') === -1 ) {
-			currentid = req.body.bib.network_number[k]
-			res.json({"currentlibrisid" : currentid});
-			break;
+
+	//Inget bibid hittades
+	if (currentid == "") {
+		//Finns ett värde som saknar "("? Då anses det vara ett "libris3" id
+		for (let k = 0; k < req.body.bib.network_number.length; k++) {
+			if(req.body.bib.network_number[k].indexOf('(') === -1 ) {
+				currentid = req.body.bib.network_number[k]
+				res.json({"currentlibrisid" : currentid});
+				response4 = await libris.findHoldinguri(currentid,'libris3')
+				break;
+			}
+			
 		}
-		//response4 = await libris.findHoldinguri(librisidarr[k],'libris3')
+	}
+	console.log(response4)
+
+	if(response4.data.totalItems > 0){
+		for (let j = 0; j < response4.data.items[0]['@reverse'].itemOf.length; j++) {
+			if(response4.data.items[0]['@reverse'].itemOf[j].heldBy['@id'] == 'https://libris.kb.se/library/T'){
+				
+				console.log(response4.data.items[0]['@reverse'].itemOf[j]['@id'])
+				const response2 = await libris.getEtag(response4.data.items[0]['@reverse'].itemOf[j]['@id'])
+				etag = response2.headers.etag
+				console.log(response2.headers.etag)
+				json_payload = JSON.stringify(response2.data)
+				console.log(response2.data)
+				
+				//const response3 = await libris.updateHolding(response4.data.items[0]['@reverse'].itemOf[j]['@id'], etag, access_token, json_payload)
+				//console.log(response3.data)
+				//const response5 = await libris.deleteHolding(response4.data.items[0]['@reverse'].itemOf[j]['@id'], etag, access_token)
+				//console.log(response5.data)
+				
+			}
+		}
+	} else {
+		console.log('No holding found!!! id:' + currentid)
 	}
 	
-
-	/*
-    if ((error)) {
-        res.status(400).send({ 'result': 'Error: ' + err});
-        return;
-    }
-    if ((!noresult)) {
-        res.status(201).send({ 'result': 'kthid ' + req.params.kthid + ' not found'});
-        return;
-    }
-    if(result) {
-        res.json({"ugusers" : results.users});
-    } else {
-        res.json({'result': 'nothing'});
-	}
-	*/
 });
 
 apiRoutes.get("/librisholding/:librisid/", VerifyToken, function(req, res, next){
